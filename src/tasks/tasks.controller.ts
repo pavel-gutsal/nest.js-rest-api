99fb75e4baf90379eed66be111e0a368c9adfe32
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-11-12 13:25:09 +0200

    master

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Query,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      // if we have filters defined
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      // return all tasks
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteById(@Param('id') id: string): Task {
+    return this.tasksService.deleteById(id);
+  }
+
+  @Put('/:id')
+  updateTask(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Task {
+    return this.tasksService.updateTask(id, updateTaskDto);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    return this.tasks.filter((el) => {
+      if (status && el.status === status) {
+        return true;
+      }
+      if (search) {
+        if (
+          el.title.toLowerCase().includes(search) ||
+          el.description.toLowerCase().includes(search)
+        ) {
+          return true;
+        }
+      }
+
+      return false;
+    });
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((el) => el.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  deleteById(id: string): Task {
+    const deleted = this.tasks.find((el) => el.id === id);
+    this.tasks = this.tasks.filter((el) => el.id !== id);
+    return deleted;
+  }
+
+  updateTask(id: string, updateTaskDto: UpdateTaskDto): Task {
+    let newObj: null | Task = null;
+
+    console.log('id', id);
+    console.log('updateTaskDto', updateTaskDto);
+
+    this.tasks = this.tasks.map((el) => {
+      if (el.id === id) {
+        newObj = { ...el, ...updateTaskDto };
+        return newObj;
+      }
+      return el;
+    });
+
+    return newObj;
+  }
+}
